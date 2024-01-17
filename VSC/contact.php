cdbc85b6b1362943e42529967492ce47c9a2b762
rassarg <156526039+rassarg@users.noreply.github.com>
2024-01-17 12:16:20 +0200

    Add files via upload

---
+++ VSC/contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Contact <?php echo $_POST["name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Message: <?php echo $_POST["message"]; ?><br>
+
+</body>
+</html>
